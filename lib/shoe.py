366503b495e08f304889d921acf2ce93ed9be377
Lynn <koliilynn@gmail.com>
2025-05-15 14:39:03 +0300

    added new code in my files

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size  # Uses the setter method below
+        self.condition = "Used"  # Initial condition (optional, but logical)
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
